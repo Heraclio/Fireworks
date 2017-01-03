47e9b6683fcd9c239846c859893f702f3a0a7bf7
LD33sonic <LD33sonic@gmail.com>
2017-01-02 20:26:53 -0600

    Added explosion logic, has a bug.

--- app/scripts/fireworks.js
+++ app/scripts/fireworks.js
@@ -29,6 +29,7 @@
   };
 
   var fireworks = [];
+
   setInterval(function(){
     context.fillStyle = "black";
     context.fillRect(0, 0, canvas.width, canvas.height);
@@ -41,89 +42,107 @@
       fireworks.push(firework);
     }
 
-    /*
-    fireworks.filter(function(firework, index){
-      var condition = firework.particle.velocity.y < 0;
-      if(firework.particle.velocity.y >= false){
-        firework.explode();
-        fireworks.splice(index, 1);
-      }
-      return condition;
-    }).map(function(firework){
-      firework.particle.draw();
-    });
-    */
-
     fireworks.filter(function(firework){
       return firework.particle.velocity.y >= 0;
     }).map(function(firework, index){
-      firework.explode();
-      fireworks.splice(index, 1);
+      if(firework.exploded) {
+        fireworks.splice(index, 1);
+      } else {
+        firework.explode();
+      }
     });
 
-    fireworks.map(function(firework){
-      firework.particle.draw();
+    fireworks.filter(function(firework){
+      return firework.particle.velocity.y < 0;
+    }).map(function(firework, index){
+      firework.particle.update().draw();
     });
-    
+
     context.fillStyle = "magenta";
     context.fillRect(0,0, particles.settings.bounds.left, canvas.height);
     context.fillRect(particles.settings.bounds.right,0, canvas.width, canvas.height);
     context.fillRect(0, particles.settings.bounds.bottom, canvas.width, canvas.height);
 
   }, 30);
-  // 30
 
   function Particle(){
     this.position = {
-      x: particles.settings.starting.x,
-      y: particles.settings.starting.y
+      x: 0,
+      y: 0
     };
-
     this.velocity = {
       x: 0,
       y: 0
     };
-
     this.life = 0;
   }
 
-  Particle.prototype.draw = function(){
+  Particle.prototype.update = function(){
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
 
     this.velocity.y += particles.settings.gravity;
     this.life++;
 
+    return this;
+  }
+
+  Particle.prototype.draw = function(){
     context.clearRect(particles.settings.bounds.left, particles.settings.bounds.bottom, canvas.width, canvas.height);
     context.fillStyle = "white";
     context.fillRect(this.position.x, this.position.y, 2, 2);
   }
 
   function Firework(){
-    this.particles = [];
-    var position = {
-        x: Math.round(Number.prototype.randomBetween(0, canvas.width) * 100) / 100
-    };
-    var velocity = {
-      x: Number.prototype.randomBetween(-2.5, 2.5)
-    };
+    var count = Math.round(Number.prototype.randomBetween(5, 10) * 1) / 1;
+    this.particles = Array.apply(null, Array(count));
 
     this.particle = new Particle();
-
-    this.particle.position.x = position.x;
-    this.particle.position.y = canvas.height;
-
-    this.particle.velocity.x = velocity.x;
-
-    var randomize = Math.round((Math.round(Math.random() * 10) / 10) * 10) / 10;
-    var min = -12.5;
-    var max = -20;
-    velocity.y = Number.prototype.randomBetween(min, max);
-    this.particle.velocity.y = velocity.y;
+    this.particle.position = {
+        x: Math.round(Number.prototype.randomBetween(0, canvas.width) * 100) / 100,
+        y: canvas.height
+    };
+    this.particle.velocity = {
+      x: Number.prototype.randomBetween(-2.5, 2.5),
+      y: Number.prototype.randomBetween(-12.5, -20)
+    };
   }
 
+  Firework.prototype.end = function(){
+    console.log(this);
+    this.exploded = true;
+  };
+
   Firework.prototype.explode = function(){
-    console.log('commence explosion sequence');
+    var position = this.particle.position;
+    var length = this.particles.length;
+    var end = this.end;
+
+    this.particles.filter(function(particle){
+      if(particle){
+        return particle.life >= 100;
+      }
+    }).map(function(particle, index){
+      console.log(particle);
+    });
+
+    // TODO: SOME BUG HERE WITH THE PARTICLES BEING EMITTED.
+    this.particles.map(function(particle, index){
+      if(!particle){
+        var randomVelocity = 4 + Math.random() * 4;
+        var angle = index * ((Math.PI * 2) / length);
+        particle = new Particle();
+        particle.position = position;
+        particle.velocity = {
+          x: Math.cos(angle) * randomVelocity,
+          y: Math.sin(angle) * randomVelocity
+        };
+      }
+      particle.update().draw();
+      particle.life++;
+
+      return particle;
+    });
   }
 
 })();
